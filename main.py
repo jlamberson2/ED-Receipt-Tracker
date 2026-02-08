213ac60d1402e81dfc28edadbf909e7ebb7434b3
Justin L <148486926+jlamberson2@users.noreply.github.com>
2026-02-08 16:50:48 -0500

    Update main.py
    adding additional entries for table on UI, functionality needs to be added to it

--- main.py
+++ main.py
@@ -1,12 +1,20 @@
 import csv
 import os
 import tkinter
+from tkinter import ttk
+from tkinter import messagebox
 
 
 #headings to the CSV and example data
-headers = ['Name','Email','Phone Number','Purchases', 'Soul Numbers']
+headers = ['Name','Email','Phone Number','Purchases', 'Soul Numbers','Payment Type', 'Sales Tax', 'Sales Fee']
 
 file_path = "currentReceipts.csv"
+
+def calculateFees():
+    print("This is a tester funciton, ERROR\n")
+
+    
+        
 
 #Adds the data inputed into a spreadsheet. if the csv does not exist, create it
 def addData(name, email, phone, purchase, souls):
@@ -30,6 +38,17 @@ def confirm_button():
     products = product_entry.get()
     souls = soul_entry.get()
 
+    #checks to make sure there is a name and email
+    if not name:
+        messagebox.showwarning('Error', 'A name needs to be attached')
+        raise Exception("Error, no name entered")
+    
+
+    if not email:
+        messagebox.showwarning('Error', 'A email needs to be attached')
+        raise Exception("Error, no email attached")
+    
+
     #add the data to the csv
     addData(name, email, phone, products, souls)
 
@@ -44,23 +63,33 @@ def confirm_button():
 
 #Base GUI code
 
+
+
 #creates the root screen
 root = tkinter.Tk()
 root.title('ED Receipt Tracker')
+root.geometry("500x300")
+
+
+#Frame for the entire UI so it can be centered
+centerFrame = tkinter.Frame(root)
+centerFrame.place(relx=0.5, rely=0.5, anchor=tkinter.CENTER)
 
 #Labels for entries
-tkinter.Label(master=root, text='Name:').grid(row=0)
-tkinter.Label(master=root, text='Email:').grid(row=1)
-tkinter.Label(master=root, text='Phone:').grid(row=2)
-tkinter.Label(master=root, text='Product:').grid(row=3)
-tkinter.Label(master=root, text='Souls:').grid(row=4)
+tkinter.Label(master=centerFrame, text='Name:').grid(row=0)
+tkinter.Label(master=centerFrame, text='Email:').grid(row=1)
+tkinter.Label(master=centerFrame, text='Phone:').grid(row=2)
+tkinter.Label(master=centerFrame, text='Product:').grid(row=3)
+tkinter.Label(master=centerFrame, text='Souls:').grid(row=4)
+tkinter.Label(master=centerFrame, text='Total Payment: ').grid(row=6)
 
 #Adding entries for the program
-name_entry = tkinter.Entry(root, width=50)
-email_entry = tkinter.Entry(root, width=50)
-phone_entry = tkinter.Entry(root, width=50)
-product_entry = tkinter.Entry(root, width=50)
-soul_entry = tkinter.Entry(root, width=50)
+name_entry = tkinter.Entry(centerFrame, width=50)
+email_entry = tkinter.Entry(centerFrame, width=50)
+phone_entry = tkinter.Entry(centerFrame, width=50)
+product_entry = tkinter.Entry(centerFrame, width=50)
+soul_entry = tkinter.Entry(centerFrame, width=50)
+payment_entry = tkinter.Entry(centerFrame, width=50)
 
 #placement for the entries
 name_entry.grid(row=0, column=1)
@@ -68,10 +97,44 @@ email_entry.grid(row=1, column=1)
 phone_entry.grid(row=2, column=1)
 product_entry.grid(row=3, column=1)
 soul_entry.grid(row=4, column=1)
+payment_entry.grid(row=6, column=1)
 
 #adding a confirm button
-confirm = tkinter.Button(root, text="Confirm", width=20, command=confirm_button)
-confirm.grid(row=5)
+confirm = tkinter.Button(centerFrame, text="Confirm", width=20, command=confirm_button)
+confirm.grid(row=10)
+
+#temp button for testing only
+tester = tkinter.Button(centerFrame, text='test', width=20, command=calculateFees)
+tester.grid(row=10, column=1)
+
+#adding in choice box for payment selection
+tkinter.Label(master=centerFrame, text='Payment Type: ').grid(row=7)
+
+#text variable for payment type
+payment_type = tkinter.StringVar()
+
+payment_selection = ttk.Combobox(master=centerFrame, width=25, textvariable=payment_type)
+
+#adding selections to combobox
+payment_selection['values'] = ('Cash', 'Paypal', 'Venmo', 'QR Code Paypal', 'Square')
+payment_selection.grid(row=7, column=1)
+payment_selection.current()
+
+# Selection of donation or product sale
+v = tkinter.StringVar(centerFrame, "1")
+
+#making a frame for the buttons
+radioContainer = tkinter.Frame(centerFrame)
+radioContainer.grid(row=8, column=1)
+
+#adding the radio buttons to the frame
+product_radio_button = tkinter.Radiobutton(master=radioContainer, text="Product Sold", variable=v, value= 1)
+donation_radio_button = tkinter.Radiobutton(master=radioContainer, text="Dontation", variable=v, value= 2)
+product_radio_button.grid(row=0, column=0)
+donation_radio_button.grid(row=0, column=1)
+
+
+
 
 #initalizes main window - NOTE: put widgets before this
 root.mainloop()
